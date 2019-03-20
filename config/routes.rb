dd8bca7552bdc09e02658621afb8750291cd473a
Guilherme <tec.guilherme.dsc@gmail.com>
2019-03-20 16:53:42 -0300

    Até capitulo 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :rooms
+  LOCALES = /en|pt\-BR/
+
+  scope "(:locale)", locale: LOCALES do
+    resources :rooms
+    resources :users    
+  end
+
+  get '/:locale' => 'home#index', locale: LOCALES
+  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
