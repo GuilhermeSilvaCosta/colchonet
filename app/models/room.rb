b8f4dd2b69b379c52fc46b504e1a6826eb0ee019
Guilherme <tec.guilherme.dsc@gmail.com>
2019-03-22 16:48:56 -0300

    Até o cap. 11.6

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,12 @@
 class Room < ApplicationRecord
+    has_many :reviews, dependent: :destroy
+    has_many :reviewed_rooms, through: :reviews, source: :room
+    belongs_to :user
     def complete_name
         "#{title}, #{location}"
+    end
+
+    def self.most_recent
+        order("created_at desc")
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
 
     EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
+    has_many :reviews, dependent: :destroy
+    has_many :rooms, dependent: :destroy
+
     validates_presence_of :email, :full_name, :location
     validates_length_of :bio, minimum: 30, allow_blank: false
     validates_format_of :email, with: EMAIL_REGEXP
